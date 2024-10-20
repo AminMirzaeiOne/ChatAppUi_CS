1847c6a96694b7d4bf4d1fdfc1e4bdf85b0f3c30
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-20 16:46:40 +0330

    - Create IsActiveProperty Filed In Item.CS

--- ChatAppUi/Controls/Item.xaml.cs
+++ ChatAppUi/Controls/Item.xaml.cs
@@ -24,6 +24,7 @@ namespace ChatAppUi.Controls
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(Item));
         public static readonly DependencyProperty TagNameProperty = DependencyProperty.Register("TagName", typeof(string), typeof(Item));
         public static readonly DependencyProperty MessageCountProperty = DependencyProperty.Register("MessageCount", typeof(string), typeof(Item));
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(Item));
 
 
         public Item()
