b961dd5b98749b3c5af0d6a8395da20a108b77dc
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-20 17:08:37 +0330

    - Create Message Property In MyMessageChat.CS

--- ChatAppUi/Controls/MyMessageChat.xaml.cs
+++ ChatAppUi/Controls/MyMessageChat.xaml.cs
@@ -22,6 +22,12 @@ namespace ChatAppUi.Controls
     {
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(MyMessageChat));
 
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
         public MyMessageChat()
         {
             InitializeComponent();
