6c7ac7f6c5307d7d5cde01cc9bb6b373f4c06fa7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-20 17:01:49 +0330

    - Create Color Property In MessageChat.CS

--- ChatAppUi/Controls/MessageChat.xaml.cs
+++ ChatAppUi/Controls/MessageChat.xaml.cs
@@ -29,6 +29,12 @@ namespace ChatAppUi.Controls
             set { SetValue(MessageProperty, value); }
         }
 
+        public Brush Color
+        {
+            get { return (Brush)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
         public MessageChat()
         {
             InitializeComponent();
