41aacf3be956fd9efc3eb59d558b648adcc650db
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-20 16:36:37 +0330

    - Create Title Property In ChatSeprator.CS

--- ChatAppUi/Controls/ChatSeprator.xaml.cs
+++ ChatAppUi/Controls/ChatSeprator.xaml.cs
@@ -22,6 +22,13 @@ namespace ChatAppUi.Controls
     {
         public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Chat_Seprator));
 
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+
         public ChatSeprator()
         {
             InitializeComponent();
